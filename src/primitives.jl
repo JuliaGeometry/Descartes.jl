9aca00128af1f7687427034166034e2eb4ba8788
Steve Kelly <kd2cca@gmail.com>
2015-04-12 19:16:22 -0400

    remove dimensionality from types and add some more primitives

--- src/primitives.jl
+++ src/primitives.jl
@@ -1,18 +1,36 @@
 
 abstract AbstractPrimitive{N, T} # dimensionality, numeric type
 
-type Cuboid{N, T} <: AbstractPrimitive{N, T}
-    dimensions::NTuple{N, T}
-    location::NTuple{N, T}
+type Cuboid{T} <: AbstractPrimitive{3, T}
+    dimensions::NTuple{3, T}
+    location::NTuple{3, T}
 end
 
-type Cylinder{N, T} <: AbstractPrimitive{N, T}
+type Cylinder{T} <: AbstractPrimitive{3, T}
     radius::T
     height::T
-    location::NTuple{N, T}
+    location::NTuple{3, T}
 end
 
-type Sphere{N, T} <: AbstractPrimitive{N, T}
+type Sphere{T} <: AbstractPrimitive{3, T}
     radius::T
-    location::NTuple{N, T}
+    location::NTuple{3, T}
+end
+
+type Ellipsoid{T} <: AbstractPrimitive{3, T}
+    dimensions::NTuple{3, T}
+    location::NTuple{3, T}
+end
+
+type EllipticCylinder{T} <: AbstractPrimitive{3, T}
+    dimensions::NTuple{2, T}
+    height::T
+    location::NTuple{3, T}
+end
+
+type PrismaticCylinder{T} <: AbstractPrimitive{3, T}
+    sides::Int
+    height::T
+    apothem::T
+    location::NTuple{3, T}
 end
--- src/transforms.jl
+++ src/transforms.jl
@@ -8,12 +8,12 @@ function (*){N,T}(translation::Translation{N,T}, obj::AbstractPrimitive{N,T})
     translate(obj, translation.location)
 end
 
-function translate{T}(c::Cuboid{3,T}, loc::NTuple{3,T})
+function translate{T}(c::Cuboid{T}, loc::NTuple{3,T})
     new_loc = (c.location[1]+loc[1], c.location[2]+loc[2], c.location[3]+loc[3])
     Cuboid{3,T}(c.dimensions, new_loc)
 end
 
-function translate{T}(c::Sphere{3,T}, loc::NTuple{3,T})
+function translate{T}(c::Sphere{T}, loc::NTuple{3,T})
     new_loc = (c.location[1]+loc[1], c.location[2]+loc[2], c.location[3]+loc[3])
     Sphere{3,T}(c.radius, new_loc)
 end
