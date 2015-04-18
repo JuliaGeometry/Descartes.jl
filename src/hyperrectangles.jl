e247b84c0e422258644b1789b63fa3129fd0623c
Steve Kelly <kd2cca@gmail.com>
2015-04-18 17:46:18 -0400

    set ops for hyperrectangles

--- src/hyperrectangles.jl
+++ src/hyperrectangles.jl
@@ -7,3 +7,15 @@ end
 function HyperRectangle{T}(sphere::Sphere{T})
     HyperRectangle{T,3}(sphere.location-sphere.radius, sphere.location+sphere.radius)
 end
+
+function HyperRectangle{N, T, L, R}(csg::CSGUnion{N, T, L, R})
+    union(HyperRectangle(csg.left), HyperRectangle(csg.right))
+end
+
+function HyperRectangle{N, T, L, R}(csg::CSGIntersect{N, T, L, R})
+    intersect(HyperRectangle(csg.left), HyperRectangle(csg.right))
+end
+
+function HyperRectangle{N, T, L, R}(csg::CSGDiff{N, T, L, R})
+    diff(HyperRectangle(csg.left), HyperRectangle(csg.right))
+end
