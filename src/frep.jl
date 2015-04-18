dfc1e107138abdfde9fa03da8baa025ba3b9b585
Steve Kelly <kd2cca@gmail.com>
2015-04-18 17:42:03 -0400

    inline FReps

--- src/frep.jl
+++ src/frep.jl
@@ -1,18 +1,18 @@
 # http://en.wikipedia.org/wiki/Function_representation
 
 
-function FRep(s::Sphere, x, y, z)
+@inline function FRep(s::Sphere, x, y, z)
     sqrt((x-s.location[1])^2 + (y-s.location[2])^2 + (z-s.location[3])^2) - s.radius
 end
 
-function FRep(u::CSGUnion, x, y, z)
+@inline function FRep(u::CSGUnion, x, y, z)
     min(FRep(u.left, x,y,z),FRep(u.right, x,y,z))
 end
 
-function FRep(u::CSGDiff, x, y, z)
+@inline function FRep(u::CSGDiff, x, y, z)
     max(FRep(u.left, x,y,z), -FRep(u.right, x,y,z))
 end
 
-function FRep(u::CSGIntersect, x, y, z)
+@inline function FRep(u::CSGIntersect, x, y, z)
     max(FRep(u.left, x,y,z), FRep(u.right, x,y,z))
 end
