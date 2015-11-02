d5534e1e8f930429efc43f5aeb4618e7b4494cf3
Steve Kelly <kd2cca@gmail.com>
2015-11-01 19:26:15 -0500

    add piped mesh function

--- src/meshes.jl
+++ src/meshes.jl
@@ -3,3 +3,14 @@ function GeometryTypes.HomogenousMesh{T}(primitive::AbstractPrimitive{3, T},
     distancefield = SignedDistanceField(primitive, resolution)
     HomogenousMesh(distancefield.data, 0.0)
 end
+
+function piped_mesh{VT,N,T,O}(m::AbstractMesh{VT, Face{N,T,O}},r)
+    c = nothing
+    for f in m.faces
+        v1 = m.vertices[f[1]-O]
+        v2 = m.vertices[f[2]-O]
+        v3 = m.vertices[f[3]-O]
+        c = CSGUnion(c, Piping(r, Point{3,Float64}[v1, v2, v3]))
+    end
+    c
+end
