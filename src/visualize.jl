968e06fb8d4738e0e5336bdf31099b20537637ef
steve <kd2cca@gmail.com>
2019-04-11 21:19:48 -0400

    fix visualizer

--- src/visualize.jl
+++ src/visualize.jl
@@ -1,18 +1,17 @@
 
-function visualize(p::AbstractPrimitive, algorithm=NaiveSurfaceNets(),
+function visualize(p::AbstractPrimitive, algorithm=MarchingCubes(),
                    res_start=1, res_scale=0.5, res_iterations=5)
 
     # first visual
     vis = Visualizer()
-    m = HomogenousMesh(p, res_start, algorithm)
+    m = HomogenousMesh(p, resolution=res_start, algorithm=algorithm)
     open(vis)
     delete!(vis) # clear visualizer
     setobject!(vis, m)
     res = res_start
     for i = 1:res_iterations
-        sleep(3)
         res = res*res_scale
-        m = HomogenousMesh(p, res)
+        m = HomogenousMesh(p, resolution=res, algorithm=algorithm)
         delete!(vis)
         setobject!(vis,m)
     end
