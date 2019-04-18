53d75b83321f41f93f1db2f6bc25b04d6729cb99
Steve Kelly <kd2cca@gmail.com>
2019-04-18 19:32:11 -0400

    wip visualizer corrections

--- src/visualize.jl
+++ src/visualize.jl
@@ -1,18 +1,19 @@
+global vis = nothing
 
-function visualize(p::AbstractPrimitive, algorithm=MarchingCubes(),
-                   res_start=1, res_scale=0.5, res_iterations=5)
+function init_vis()
+    if vis == nothing
+        vis = Visualizer()
+    end
+    vis
+end
+
+
+function visualize(m::AbstractMesh, algorithm=MarchingCubes(),
+                   resolution=1)
 
     # first visual
-    vis = Visualizer()
-    m = HomogenousMesh(p, resolution=res_start, algorithm=algorithm)
+    vis = init_vis()
     open(vis)
     delete!(vis) # clear visualizer
     setobject!(vis, m)
-    res = res_start
-    for i = 1:res_iterations
-        res = res*res_scale
-        m = HomogenousMesh(p, resolution=res, algorithm=algorithm)
-        delete!(vis)
-        setobject!(vis,m)
-    end
 end
