b507afea21229dec8467cdcbf1d7bc57bcc29ab2
Steve Kelly <kd2cca@gmail.com>
2019-05-19 21:04:01 -0400

    use float32 svector for frep computation

--- src/distancefield.jl
+++ src/distancefield.jl
@@ -1,10 +1,12 @@
 function SignedDistanceField(primitive::AbstractPrimitive{3,T},
                                 resolution=0.1) where {T}
     bounds = HyperRectangle(primitive)
-    x_min, y_min, z_min = minimum(bounds)
-    x_max, y_max, z_max = maximum(bounds)
+    min_b = SVector{3,Float32}(minimum(bounds))
+    max_b = SVector{3,Float32}(maximum(bounds))
+    x_min, y_min, z_min = min_b
+    x_max, y_max, z_max = max_b
 
-    x_rng, y_rng, z_rng = maximum(bounds) - minimum(bounds)
+    x_rng, y_rng, z_rng = max_b - min_b
 
     nx = ceil(Int, x_rng/resolution)
     ny = ceil(Int, y_rng/resolution)
@@ -20,7 +22,7 @@ function SignedDistanceField(primitive::AbstractPrimitive{3,T},
     bounds = HyperRectangle(o..., w...)
 
     for i = 0:nx, j = 0:ny, k = 0:nz
-        vec = SVector(x_min + resolution*i,
+        vec = SVector{3,Float32}(x_min + resolution*i,
                       y_min + resolution*j,
                       z_min + resolution*k)
         @inbounds vol[i+1,j+1,k+1] = FRep(primitive,vec)
