c3a014f5baef435315bded0fa5d9eb58877cfcb9
Steve Kelly <kd2cca@gmail.com>
2019-05-26 10:21:58 -0400

    additional fixes, tri generation not ported correctly

--- src/dual_contours.jl
+++ src/dual_contours.jl
@@ -3,6 +3,10 @@
 #import scipy.optimize as opt
 #import itertools as it
 using LinearAlgebra
+using Roots
+using GeometryTypes
+using FileIO
+using StaticArrays
 
 #Cardinal directions
 dirs = [ [1,0,0], [0,1,0], [0,0,1] ]
@@ -20,7 +24,8 @@ cube_edges = [(0, 1), (0, 2), (0, 1), (0, 4), (0, 2), (0, 4), (2, 3), (1, 3),
 
 #Use non-linear root finding to compute intersection point
 function estimate_hermite(f, df, v0, v1)
-    t0 = opt.brentq(t -> f((1.0-t)*v0 + t*v1), 0, 1)
+    t0 = find_zero(t -> f((1.0-t)*v0 + t*v1),
+                    (0, 1))
     x0 = (1.0-t0)*v0 + t0*v1
     return (x0, df(x0))
 end
@@ -48,9 +53,12 @@ function dual_contour(f, df, nc)
         h_data = [ estimate_hermite(f, df, o+cube_verts[e[1]+1], o+cube_verts[e[2]+1])
             for e in cube_edges if cube_signs[e[1]+1] != cube_signs[e[2]+1] ]
 
+        @show h_data
         #Solve qef to get vertex
-        A = [ d[1] for d in h_data ]
+        A = reshape(vcat([ d[2] for d in h_data ]...),length(h_data),:)
+        @show A
         b = [ dot(d[1],d[2]) for d in h_data ]
+        @show b
         v = A\b
 
         #Throw out failed solutions
@@ -59,14 +67,14 @@ function dual_contour(f, df, nc)
         end
 
         #Emit one vertex per every cube that crosses
-        vindex[ o ] = length(dc_verts)
-        append(dc_verts, v)
+        push!(vindex, o => length(dc_verts))
+        push!(dc_verts, v)
     end
 
     #Construct faces
     dc_faces = []
     for x in 0:nc, y in 0:nc, z in 0:nc
-        if !in((x,y,z),vindex)
+        if !haskey(vindex,[x,y,z])
             continue
         end
 
@@ -74,9 +82,9 @@ function dual_contour(f, df, nc)
         o = [x,y,z]
         for i in (0,1,2)
             for j in 0:i
-                if o + dirs[i] in vindex && o + dirs[j] in vindex && o + dirs[i] + dirs[j] in vindex
-                    append(dc_faces, [vindex[tuple(o)], vindex[tuple(o+dirs[i])], vindex[tuple(o+dirs[j])]] )
-                    append(dc_faces, [vindex[tuple(o+dirs[i]+dirs[j])], vindex[tuple(o+dirs[j])], vindex[tuple(o+dirs[i])]] )
+                if haskey(vindex,o + dirs[i+1]) && haskey(vindex,o + dirs[j+1]) && haskey(vindex,o + dirs[i+1] + dirs[j+1])
+                    push!(dc_faces, [vindex[o], vindex[o+dirs[i+1]], vindex[o+dirs[j+1]]] )
+                    push!(dc_faces, [vindex[o+dirs[i+1]+dirs[j+1]], vindex[o+dirs[j+1]], vindex[o+dirs[i+1]]] )
                 end
             end
         end
@@ -101,4 +109,8 @@ end
 
 verts, tris = dual_contour(test_f, test_df, 36)
 
-m = HomogenousMesh(verts, tris)
+@show tris
+
+m = HomogenousMesh([Point(v...) for v in verts], [SVector(t...) for t in tris])
+
+save("test.ply",m)
