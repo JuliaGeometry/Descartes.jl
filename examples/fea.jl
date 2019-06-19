7134c53c9da05e1fb84e0176c4ee183f75dc5ade
Steve Kelly <kd2cca@gmail.com>
2019-06-18 21:10:04 -0400

    init file for fea

---
+++ examples/fea.jl
@@ -0,0 +1,20 @@
+using Descartes
+
+# params
+beam_size = [50,10,10]
+hole_ct = 5
+hole_d = 3
+
+# computations
+hole_interval = beam_size[1]/(hole_ct + 1)
+
+c = Cuboid(beam_size)
+
+for i = 1:hole_ct
+    h = translate([hole_interval*i, -1, beam_size[3]/2])*
+            rotate(-pi/2, [1,0,0])*
+                Cylinder(hole_d/2, beam_size[2]+2, center=false)
+    global c = CSGDiff(c, h)
+end
+
+save("fea.ply",HomogenousMesh(c, cl=false, resolution=0.5))
