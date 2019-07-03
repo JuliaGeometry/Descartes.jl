538db7cfae3c70a5b27a28266127ca7c76c11ed6
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-07-03 01:00:58 +0000

    build based on 55d633a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Descartes.jl-1","page":"Descartes.jl","title":"Descartes.jl","text":"","category":"section"},{"location":"#","page":"Descartes.jl","title":"Descartes.jl","text":"Documentation for Descartes.jl","category":"page"}]
+}
