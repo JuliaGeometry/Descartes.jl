280e66a1b9f8d64d8a0d9ebb4bfd7eb54e3c20b7
Steve Kelly <kd2cca@gmail.com>
2019-11-01 20:00:37 -0400

    plot tweaks

--- examples/fea.jl
+++ examples/fea.jl
@@ -83,15 +83,15 @@ statsdata
 using Plots
 
 qual_hist = Plots.histogram(qualities, title = "Quality", legend=false)
-avg_plt = Plots.plot(statsdata.average_qual, title = "Average Quality", legend=false)
+avg_plt = Plots.plot(statsdata.average_qual, title = "Average Quality", legend=false, ylabel="Quality")
 vline!(statsdata.retriangulations, line=(0.2, :dot, [:red]))
-med_plt = Plots.plot(statsdata.median_qual, title = "Median Quality", legend=false)
+med_plt = Plots.plot(statsdata.median_qual, title = "Median Quality", legend=false, ylabel="Quality")
 vline!(statsdata.retriangulations, line=(0.2, :dot, [:red]))
-maxdp_plt = Plots.plot(statsdata.maxdp, title = "Max Displacement", legend=false)
+maxdp_plt = Plots.plot(statsdata.maxdp, title = "Max Displacement", legend=false, ylabel="Edge Displacement")
 vline!(statsdata.retriangulations, line=(0.2, :dot, [:red]))
-maxmove_plt = Plots.plot(statsdata.maxmove, title = "Max Move", legend=false)
+maxmove_plt = Plots.plot(statsdata.maxmove, title = "Max Move", legend=false, ylabel="Point Displacement")
 vline!(statsdata.retriangulations, line=(0.2, :dot, [:red]))
-plt = Plots.plot(avg_plt, med_plt,maxdp_plt,maxmove_plt,layout=(2,2))
+plt = Plots.plot(avg_plt, med_plt,maxdp_plt,maxmove_plt,layout=(2,2), xlabel="Iteration", )
 savefig(plt, "result_stat.svg")
 savefig(qual_hist, "result_qual.svg")
 
