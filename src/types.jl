c9313c819627bf3dd4db5e198129908a577f5505
Steve Kelly <kd2cca@gmail.com>
2020-05-13 18:13:32 -0400

    initial pass at supporting CoordinateTransformations

--- src/types.jl
+++ src/types.jl
@@ -1,21 +1,16 @@
 abstract type AbstractPrimitive{N, T} end # dimensionality, numeric type
 abstract type AbstractCSGTree{N,T} <: AbstractPrimitive{N, T} end
 abstract type AbstractOperation{N,T} <: AbstractPrimitive{N,T} end
-abstract type AbstractTransform{N,T} end
 
 # 2D Geometric Primitives
 
 struct Square{T} <: AbstractPrimitive{2, T}
     dimensions::SVector{2,T}
     lowercorner::SVector{2,T}
-    transform::SMatrix{3,3,T,9}
-    inv_transform::SMatrix{3,3,T,9}
 end
 
 struct Circle{T} <: AbstractPrimitive{2, T}
     radius::T
-    transform::SMatrix{3,3,T,9}
-    inv_transform::SMatrix{3,3,T,9}
 end
 
 # 3D Geometric Primitives
@@ -23,35 +18,29 @@ end
 struct Cuboid{T} <: AbstractPrimitive{3, T}
     dimensions::SVector{3,T}
     lowercorner::SVector{3,T}
-    transform::SMatrix{4,4,T,16}
-    inv_transform::SMatrix{4,4,T,16}
 end
 
 struct Cylinder{T} <: AbstractPrimitive{3, T}
     radius::T
     height::T
     bottom::T
-    transform::SMatrix{4,4,T,16}
-    inv_transform::SMatrix{4,4,T,16}
 end
 
 struct Sphere{T} <: AbstractPrimitive{3, T}
     radius::T
-    transform::SMatrix{4,4,T,16}
-    inv_transform::SMatrix{4,4,T,16}
 end
 
 struct Piping{T} <: AbstractPrimitive{3, T}
     radius::T
     points::Vector{SVector{3,T}}
-    transform::SMatrix{4,4,T,16}
-    inv_transform::SMatrix{4,4,T,16}
 end
 
 # transforms
 
-struct Transform{N,T} <: AbstractTransform{N,T}
-    transform::SMatrix{N,N,T} #TODO: where L?
+struct MapContainer{N, T, M, P<:AbstractPrimitive{N,T}} <: AbstractPrimitive{N,T}
+    map::M
+    inv::M
+    primitive::P
 end
 
 
@@ -89,6 +78,4 @@ end
 struct LinearExtrude{N, T, P} <: AbstractPrimitive{3,T}
     primitive::P
     distance::T
-    transform::SMatrix{4,4,T,16}
-    inv_transform::SMatrix{4,4,T,16}
 end
