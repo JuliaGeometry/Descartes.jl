0c5bb1da4b16679e9bab61af9b5267a22fa307ee
Vedant Puri <vedantpuri@gmail.com>
2021-10-18 12:43:19 -0400

    vectorize frep calls

--- src/frep.jl
+++ src/frep.jl
@@ -17,25 +17,62 @@ function FRep(p::MapContainer{2,T,P}, v) where {T,P}
     FRep(p.primitive, p.inv(SVector(v[1],v[2])))
 end
 
-function FRep(p::Sphere, v)
-    x = v[1]
-    y = v[2]
-    z = v[3]
-    sqrt(x*x + y*y + z*z) - p.radius
+#----------------------------------
+function FRep(p::Sphere,x::AbstractArray,y::AbstractArray,z::AbstractArray)
+    @assert size(x) == size(y) == size(z)
+    @. sqrt(x*x + y*y + z*z) - p.radius
 end
 
-function FRep(p::Cylinder, v)
+function FRep(p::Sphere,xyz::AbstractArray)
+    @assert size(xyz)[1] == 3
+    r2 = sum(xyz .* xyz,dims=1)
+    @. sqrt(r2) - p.radius
+end
+#----------------------------------
+function FRep(p::Cylinder,v::AbstractVector)
     x = v[1]
     y = v[2]
     z = v[3]
     max(-z+p.bottom, z-p.height-p.bottom, sqrt(x*x + y*y) - p.radius)
 end
 
-function FRep(p::Circle, v)
+function FRep(p::Cylinder,x::AbstractArray,y::AbstractArray,z::AbstractArray)
+    @assert size(x) == size(y) == size(z)
+    r = @. sqrt(x*x + y*y) - p.radius
+    a = @. -z+p.bottom
+    b = @.  z-(p.height+p.bottom)
+    max.(a, b, r)
+end
+
+function FRep(p::Cylinder,xyz::AbstractArray)
+    @assert size(xyz)[1] == 3
+    sz = size(xyz)
+    xyz = reshape(xyz,3,:)
+
+    x = xyz[1,:]
+    y = xyz[2,:]
+    z = xyz[3,:]
+    fr = FRep(p,x,y,z)
+
+    reshape(fr,1,sz[2:end]...)
+end
+#----------------------------------
+function FRep(p::Circle, v::AbstractVector)
     norm(v) - p.radius
 end
 
-function FRep(p::Cuboid, v)
+function FRep(p::Circle,x::AbstractArray,y::AbstractArray)
+    @assert size(x) == size(y)
+    @. sqrt(x*x + y*y) - p.radius
+end
+
+function FRep(p::Circle,xy::AbstractMatrix)
+    @assert size(xy)[1] == 2
+    r2 = sum(xy .* xy,dims=1)
+    @. sqrt(r2) - p.radius
+end
+#----------------------------------
+function FRep(p::Cuboid, v::AbstractVector)
     x = v[1]
     y = v[2]
     z = v[3]
@@ -46,7 +83,33 @@ function FRep(p::Cuboid, v)
         -z+lbz, z-dz-lbz)
 end
 
-function FRep(p::Square, v)
+function FRep(p::Cuboid,x::AbstractArray,y::AbstractArray,z::AbstractArray)
+    @assert size(x) == size(y) == size(z)
+    dx, dy, dz = p.dimensions
+    lbx, lby,lbz = p.lowercorner
+    a = @. -x + lbx
+    b = @.  x - (dx+lbx)
+    c = @. -y + lby
+    d = @.  y - (dy+lby)
+    e = @. -z + lbz
+    f = @.  z - (dz+lbz)
+    max.(a,b,c,d,e,f)
+end
+
+function FRep(p::Cuboid,xyz::AbstractArray)
+    @assert size(xyz)[1] == 3
+    sz  = size(xyz)
+    xyz = reshape(xyz,3,:)
+
+    x = xyz[1,:]
+    y = xyz[2,:]
+    z = xyz[3,:]
+    fr = FRep(p,x,y,z)
+
+    reshape(fr,1,sz[2:end]...)
+end
+#----------------------------------
+function FRep(p::Square, v::AbstractVector)
     x = v[1]
     y = v[2]
     dx, dy = p.dimensions
@@ -54,6 +117,31 @@ function FRep(p::Square, v)
     max(-x+lbx, x-dx-lbx,
         -y+lby, y-dy-lby)
 end
+
+function FRep(p::Square,x::AbstractArray,y::AbstractArray)
+    @assert size(x) == size(y)
+    dx, dy = p.dimensions
+    lbx, lby = p.lowercorner
+    a = @. -x + lbx
+    b = @.  x - (dx+lbx)
+    c = @. -y + lby
+    d = @.  y - (dy+lby)
+
+    max.(a,b,c,d)
+end
+
+function FRep(p::Square,xy::AbstractArray)
+    @assert size(xy)[1] == 2
+    sz = size(xy)
+    xy = reshape(xy,2,:)
+
+    x = xy[1,:]
+    y = xy[2,:]
+    fr = FRep(p,x,y)
+
+    reshape(fr,1,sz[2:end]...)
+end
+#----------------------------------
 
 function FRep(u::CSGUnion, v)
     min(FRep(u.left, v),FRep(u.right, v))
