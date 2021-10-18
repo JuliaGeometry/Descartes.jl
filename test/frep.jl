5a94ddc95711fc00a8550b6ccae8ee94afc7f5c5
Vedant Puri <vedantpuri@gmail.com>
2021-10-18 17:07:21 -0400

    tests

--- test/frep.jl
+++ test/frep.jl
@@ -1,44 +1,46 @@
 #
-using Descartes
+@testset "FReps" begin
+    N = 10
+    xy  = rand(2,N)
+    xyz = rand(3,N)
 
-#----------------------------------
-p  = Circle(1.0)
-xy = rand(2,10)
+    x = rand(N,N,N)
+    y = rand(N,N,N)
+    z = rand(N,N,N)
+    #----------------------------------
 
-@show f = FRep(p,xy[:,1]) |> size
-@show f = FRep(p,xy) |> size
-@show f = FRep(p,xy[1,:],xy[2,:]) |> size
-#----------------------------------
-
-p   = Sphere(1.0)
-xyz = rand(3,10)
-
-@show f = FRep(p,xyz[:,1]) |> size
-@show f = FRep(p,xyz) |> size
-@show f = FRep(p,xyz[1,:],xyz[2,:],xyz[3,:]) |> size
-#----------------------------------
-
-p   = Square([1.0,1.0],center=true)
-xyz = rand(3,10)
-
-@show f = FRep(p,xy[:,1]) |> size
-@show f = FRep(p,xy) |> size
-@show f = FRep(p,xy[1,:],xy[2,:]) |> size
-#----------------------------------
-
-p   = Cuboid([1.0,1.0,1.0],center=true)
-xyz = rand(3,10)
-
-@show f = FRep(p,xyz[:,1]) |> size
-@show f = FRep(p,xyz) |> size
-@show f = FRep(p,xyz[1,:],xyz[2,:],xyz[3,:]) |> size
-#----------------------------------
-
-p   = Cylinder(1.0,1.0,1.0)
-xyz = rand(3,10)
-
-@show f = FRep(p,xyz[:,1]) |> size
-@show f = FRep(p,xyz) |> size
-@show f = FRep(p,xyz[1,:],xyz[2,:],xyz[3,:]) |> size
-#----------------------------------
-return
+    p  = Circle(1.0)
+    
+    @test FRep(p,xy[:,1]) |> size == ()
+    @test FRep(p,xy)      |> size == (1,N)
+    @test FRep(p,x,y)     |> size == (N,N,N)
+    #----------------------------------
+    
+    p = Square([1.0,1.0],center=true)
+    
+    @test FRep(p,xy[:,1]) |> size == ()
+    @test FRep(p,xy)      |> size == (1,N)
+    @test FRep(p,x,y)     |> size == (N,N,N)
+    #----------------------------------
+    
+    p = Sphere(1.0)
+    
+    @test FRep(p,xyz[:,1]) |> size == ()
+    @test FRep(p,xyz)      |> size == (1,N)
+    @test FRep(p,x,y,z)    |> size == (N,N,N)
+    #----------------------------------
+    
+    p = Cuboid([1.0,1.0,1.0],center=true)
+    
+    @test FRep(p,xyz[:,1]) |> size == ()
+    @test FRep(p,xyz)      |> size == (1,N)
+    @test FRep(p,x,y,z)    |> size == (N,N,N)
+    #----------------------------------
+    
+    p = Cylinder(1.0,1.0,1.0)
+    
+    @test FRep(p,xyz[:,1]) |> size == ()
+    @test FRep(p,xyz)      |> size == (1,N)
+    @test FRep(p,x,y,z)    |> size == (N,N,N)
+    #----------------------------------
+end
