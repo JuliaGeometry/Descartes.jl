7e0fafcd0cfde8c4056fb5a7e4497ea55303e6e3
Vedant Puri <vedantpuri@gmail.com>
2021-10-19 13:24:39 -0400

    linear extrude

--- src/frep.jl
+++ src/frep.jl
@@ -1,14 +1,20 @@
 # http://en.wikipedia.org/wiki/Function_representation
-
 #----------------------------------
 
-function FRep(p::MapContainer{3,T,P}, v) where {T,P}
+function FRep(p::MapContainer{N,T,P}, v) where {N,T,P}
     FRep(p.primitive, p.inv(v))
 end
 
+function FRep(u::CSGUnion, v)
+    min.(FRep(u.left, v),FRep(u.right, v))
+end
 
-function FRep(p::MapContainer{2,T,P}, v) where {T,P}
-    FRep(p.primitive, p.inv(SVector(v[1],v[2])))
+function FRep(u::CSGDiff, v)
+    max.(FRep(u.left, v), -FRep(u.right, v))
+end
+
+function FRep(u::CSGIntersect, v)
+    max.(FRep(u.left, v), FRep(u.right, v))
 end
 #----------------------------------
 
@@ -16,12 +22,12 @@ function FRep(p::Sphere,v::AbstractVector)
     norm(v) - p.radius
 end
 
-function FRep(p::Sphere,x::AbstractArray,y::AbstractArray,z::AbstractArray)
+function FRep(p::Sphere,x::AbstractMatrix,y::AbstractMatrix,z::AbstractMatrix)
     @assert size(x) == size(y) == size(z)
     @. sqrt(x*x + y*y + z*z) - p.radius
 end
 
-function FRep(p::Sphere,xyz::AbstractArray)
+function FRep(p::Sphere,xyz::AbstractMatrix)
     @assert size(xyz)[1] == 3
     r2 = sum(xyz .* xyz,dims=1)
     @. sqrt(r2) - p.radius
@@ -35,7 +41,7 @@ function FRep(p::Cylinder,v::AbstractVector)
     max(-z+p.bottom, z-p.height-p.bottom, sqrt(x*x + y*y) - p.radius)
 end
 
-function FRep(p::Cylinder,x::AbstractArray,y::AbstractArray,z::AbstractArray)
+function FRep(p::Cylinder,x::AbstractMatrix,y::AbstractMatrix,z::AbstractMatrix)
     @assert size(x) == size(y) == size(z)
     r = @. sqrt(x*x + y*y) - p.radius
     a = @. -z+p.bottom
@@ -43,17 +49,13 @@ function FRep(p::Cylinder,x::AbstractArray,y::AbstractArray,z::AbstractArray)
     max.(a, b, r)
 end
 
-function FRep(p::Cylinder,xyz::AbstractArray)
+function FRep(p::Cylinder,xyz::AbstractMatrix)
     @assert size(xyz)[1] == 3
-    sz = size(xyz)
-    xyz = reshape(xyz,3,:)
 
-    x = xyz[1,:]
-    y = xyz[2,:]
-    z = xyz[3,:]
+    x = xyz[1,:]'
+    y = xyz[2,:]'
+    z = xyz[3,:]'
     fr = FRep(p,x,y,z)
-
-    reshape(fr,1,sz[2:end]...)
 end
 #----------------------------------
 
@@ -61,12 +63,12 @@ function FRep(p::Circle, v::AbstractVector)
     norm(v) - p.radius
 end
 
-function FRep(p::Circle,x::AbstractArray,y::AbstractArray)
+function FRep(p::Circle,x::AbstractMatrix,y::AbstractMatrix)
     @assert size(x) == size(y)
     @. sqrt(x*x + y*y) - p.radius
 end
 
-function FRep(p::Circle,xy::AbstractArray)
+function FRep(p::Circle,xy::AbstractMatrix)
     @assert size(xy)[1] == 2
     r2 = sum(xy .* xy,dims=1)
     @. sqrt(r2) - p.radius
@@ -84,7 +86,7 @@ function FRep(p::Cuboid, v::AbstractVector)
         -z+lbz, z-dz-lbz)
 end
 
-function FRep(p::Cuboid,x::AbstractArray,y::AbstractArray,z::AbstractArray)
+function FRep(p::Cuboid,x::AbstractMatrix,y::AbstractMatrix,z::AbstractMatrix)
     @assert size(x) == size(y) == size(z)
     dx, dy, dz = p.dimensions
     lbx, lby,lbz = p.lowercorner
@@ -97,17 +99,13 @@ function FRep(p::Cuboid,x::AbstractArray,y::AbstractArray,z::AbstractArray)
     max.(a,b,c,d,e,f)
 end
 
-function FRep(p::Cuboid,xyz::AbstractArray)
+function FRep(p::Cuboid,xyz::AbstractMatrix)
     @assert size(xyz)[1] == 3
-    sz  = size(xyz)
-    xyz = reshape(xyz,3,:)
 
-    x = xyz[1,:]
-    y = xyz[2,:]
-    z = xyz[3,:]
+    x = xyz[1,:]'
+    y = xyz[2,:]'
+    z = xyz[3,:]'
     fr = FRep(p,x,y,z)
-
-    reshape(fr,1,sz[2:end]...)
 end
 #----------------------------------
 
@@ -120,7 +118,7 @@ function FRep(p::Square, v::AbstractVector)
         -y+lby, y-dy-lby)
 end
 
-function FRep(p::Square,x::AbstractArray,y::AbstractArray)
+function FRep(p::Square,x::AbstractMatrix,y::AbstractMatrix)
     @assert size(x) == size(y)
     dx, dy = p.dimensions
     lbx, lby = p.lowercorner
@@ -132,34 +130,18 @@ function FRep(p::Square,x::AbstractArray,y::AbstractArray)
     max.(a,b,c,d)
 end
 
-function FRep(p::Square,xy::AbstractArray)
+function FRep(p::Square,xy::AbstractMatrix)
     @assert size(xy)[1] == 2
-    sz = size(xy)
-    xy = reshape(xy,2,:)
 
-    x = xy[1,:]
-    y = xy[2,:]
+    x = xy[1,:]'
+    y = xy[2,:]'
     fr = FRep(p,x,y)
-
-    reshape(fr,1,sz[2:end]...)
 end
 #----------------------------------
 
-function FRep(u::CSGUnion, v)
-    min.(FRep(u.left, v),FRep(u.right, v))
-end
-
-function FRep(u::CSGDiff, v)
-    max.(FRep(u.left, v), -FRep(u.right, v))
-end
-
-function FRep(u::CSGIntersect, v)
-    max.(FRep(u.left, v), FRep(u.right, v))
-end
-
 function FRep(s::Shell, v)
     r = FRep(s.primitive, v)
-    max.(r, -r-s.distance)
+    max.(r, -r .- s.distance)
 end
 
 #----------------------------------
@@ -208,29 +190,53 @@ function FRep(p::Piping{T}, v::AbstractVector) where {T}
     end
     val - p.radius
 end
+
+#function FRep(p::Piping{T}, v::AbstractVector) where {T}
+#    num_pts = length(p.points)
+#
+#    val = typemax(T)
+#
+#    for i = 1:num_pts-1
+#        e1 = p.points[i]
+#        e2 = p.points[i+1]
+#        v = e2 - e1
+#        w = v - e1
+#        if dot(w,v) <= 0
+#            nv = norm(v - e1)
+#        elseif dot(v,v) <= dot(w,v)
+#            nv = norm(v - e2)
+#        else
+#            nv = norm(cross(v-e1,v-e2))/norm(e2-e1)
+#        end
+#        val = min(nv, val)
+#    end
+#    val - p.radius
+#end
 #----------------------------------
 
 function FRep(p::LinearExtrude, v::AbstractVector)
     x = v[1]
     y = v[2]
     z = v[3]
-    r = FRep(p.primitive, v) # input should be a 2D vector
+    r = FRep(p.primitive, [x,y])
     max.(max.(-z,z-p.distance), r)
 end
 
-function FRep(p::LinearExtrude,x::AbstractArray,y::AbstractArray,z::AbstractArray)
+function FRep(p::LinearExtrude,x::AbstractMatrix,y::AbstractMatrix,z::AbstractMatrix)
     @assert size(x) == size(y) == size(z)
 
-    r = FRep(p.Primitive,x,y)
+    xy = vcat(x,y)
+    r = FRep(p.primitive,xy)
     @. max(max(-z,z-p.distance), r)
 end
 
-function FRep(p::LinearExtrude,xyz::AbstractArray)
+function FRep(p::LinearExtrude,xyz::AbstractMatrix)
     @assert size(xyz)[1] == 3
 
-    x = xyz[1,:]
-    y = xyz[2,:]
-    z = xyz[3,:]
-    FRep(p,x,y,z)
+    x = xyz[1,:]'
+    y = xyz[2,:]'
+    z = xyz[3,:]'
+    fr = FRep(p,x,y,z)
+
 end
 #----------------------------------
--- test/frep.jl
+++ test/frep.jl
@@ -4,43 +4,50 @@
     xy  = rand(2,N)
     xyz = rand(3,N)
 
-    x = rand(N,N,N)
-    y = rand(N,N,N)
-    z = rand(N,N,N)
+    x = rand(1,N)
+    y = rand(1,N)
+    z = rand(1,N)
     #----------------------------------
 
     p  = Circle(1.0)
     
     @test FRep(p,xy[:,1]) |> size == ()
     @test FRep(p,xy)      |> size == (1,N)
-    @test FRep(p,x,y)     |> size == (N,N,N)
+    @test FRep(p,x,y)     |> size == (1,N)
     #----------------------------------
     
     p = Square([1.0,1.0],center=true)
     
     @test FRep(p,xy[:,1]) |> size == ()
     @test FRep(p,xy)      |> size == (1,N)
-    @test FRep(p,x,y)     |> size == (N,N,N)
+    @test FRep(p,x,y)     |> size == (1,N)
     #----------------------------------
     
     p = Sphere(1.0)
     
     @test FRep(p,xyz[:,1]) |> size == ()
     @test FRep(p,xyz)      |> size == (1,N)
-    @test FRep(p,x,y,z)    |> size == (N,N,N)
+    @test FRep(p,x,y,z)    |> size == (1,N)
     #----------------------------------
     
     p = Cuboid([1.0,1.0,1.0],center=true)
     
     @test FRep(p,xyz[:,1]) |> size == ()
     @test FRep(p,xyz)      |> size == (1,N)
-    @test FRep(p,x,y,z)    |> size == (N,N,N)
+    @test FRep(p,x,y,z)    |> size == (1,N)
     #----------------------------------
     
     p = Cylinder(1.0,1.0,1.0)
     
     @test FRep(p,xyz[:,1]) |> size == ()
     @test FRep(p,xyz)      |> size == (1,N)
-    @test FRep(p,x,y,z)    |> size == (N,N,N)
+    @test FRep(p,x,y,z)    |> size == (1,N)
+    #----------------------------------
+    
+    p = LinearExtrude(Circle(1.0),2.0)
+    
+    @test FRep(p,xyz[:,1]) |> size == ()
+    @test FRep(p,xyz)      |> size == (1,N)
+    @test FRep(p,x,y,z)    |> size == (1,N)
     #----------------------------------
 end
